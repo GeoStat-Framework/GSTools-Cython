e9de9c989b5a41201188c39a4ea54bce38f4169f
Sebastian Müller <mueller.seb@posteo.de>
2024-07-12 21:47:42 +0200

    prepare tests

---
+++ tests/test_variogram.py
@@ -0,0 +1,27 @@
+"""
+This is a unittest of the variogram module.
+"""
+
+import unittest
+
+import numpy as np
+
+import gstools_cython as gs_cy
+
+
+class TestVariogram(unittest.TestCase):
+    def test_directional(self):
+         ...
+
+    def test_unstructured(self):
+         ...
+
+    def test_structured(self):
+         ...
+
+    def test_ma_structured(self):
+         ...
+
+
+if __name__ == "__main__":
+    unittest.main()
