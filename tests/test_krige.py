753b935beb1dcd726f908a7781c1b32be4993b33
Sebastian Müller <mueller.seb@posteo.de>
2024-07-13 12:23:16 +0200

    add krige tests

--- tests/test_krige.py
+++ tests/test_krige.py
@@ -10,9 +10,39 @@ import gstools_cython as gs_cy
 
 
 class TestKrige(unittest.TestCase):
-    def test_calc_field_krige_and_variance(self): ...
+    def setUp(self):
 
-    def test_calc_field_krige(self): ...
+        self.krig_mat = np.array(
+            [
+                [22.779309008408386, 17.71701030060681, -35.714164777816634],
+                [17.717010300606795, 22.779309008408426, -35.714164777816656],
+                [-35.71416477781662, -35.71416477781667, 64.9934565679449],
+            ],
+            dtype=np.double,
+        )
+        self.krig_vecs = np.array(
+            [
+                [0.49608839014628076, 0.37685660597823356],
+                [0.34027802306393057, 0.4845362131524053],
+                [0.4658772855496882, 0.4845362131524053],
+            ],
+            dtype=np.double,
+        )
+        self.cond = np.array([-0.53, -0.43999999999999995, -0.26], dtype=np.double)
+
+        self.field_ref = np.array([-0.42936306, -0.29739613], dtype=np.double)
+        self.error_ref = np.array([0.49987232, 0.49982352], dtype=np.double)
+
+    def test_calc_field_krige_and_variance(self):
+        field, error = gs_cy.krige.calc_field_krige_and_variance(
+            self.krig_mat, self.krig_vecs, self.cond
+        )
+        np.testing.assert_allclose(field, self.field_ref)
+        np.testing.assert_allclose(error, self.error_ref)
+
+    def test_calc_field_krige(self):
+        field = gs_cy.krige.calc_field_krige(self.krig_mat, self.krig_vecs, self.cond)
+        np.testing.assert_allclose(field, self.field_ref)
 
 
 if __name__ == "__main__":
